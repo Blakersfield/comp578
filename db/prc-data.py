aaab689aa3c34ed7c297fa4eaba84c8741089b96
Blake <blake.pinette@gmail.com>
2022-04-27 11:55:15 -0700

    grab compound sentiment for that metric and include in file

--- db/prc-data.py
+++ db/prc-data.py
@@ -4,6 +4,9 @@ from pandas import json_normalize
 from nltk.sentiment import SentimentIntensityAnalyzer
 import pandas as pd
 
+
+AGGREGATE_OFFSET = '1D'
+
 def retrieve_data(db = 'comp578',coll="Tweets"):
     """Retrieve data from the db
         db:     name of target database\
@@ -19,8 +22,9 @@ data_flat = json_normalize(retrieve_data())
 
 def analyze(text):
     scores = sia.polarity_scores(text)
-    return max(scores,key=scores.get)
+    return scores['compound']
 
+#clean up the data
 data_flat['text']= data_flat['text'].apply(analyze)
 data_flat.drop(labels=['_id','author_id'],axis=1,inplace=True)
 data_flat.columns=[
@@ -31,27 +35,29 @@ data_flat.columns=[
 'reply_count',
 'like_count',
 'quote_count']
+
+#set datetime index
 date_time = pd.to_datetime(data_flat['created_at']).dt.strftime('%Y-%m-%dT%H:%M:%SZ')
-#print(date_time)
 datetime_index = pd.DatetimeIndex(date_time)
-#print(datetime_index)
 data_flat.set_index(keys=datetime_index,drop=True,inplace=True)
 
-
-#print(type(data_flat['created_at'][1]))
-#print(data_flat)
-reply_count = data_flat['reply_count'].resample('1D').sum()
-retweet_count = data_flat['retweet_count'].resample('1D').sum()
-like_count = data_flat['like_count'].resample('1D').sum()
-quote_count = data_flat['quote_count'].resample('1D').sum()
-tweet_count = data_flat['text'].resample('1D').count()
+#aggregate based on day
+text_sent = data_flat['text'].resample(AGGREGATE_OFFSET).mean()
+text_sent.name = 'sentiment'
+reply_count = data_flat['reply_count'].resample(AGGREGATE_OFFSET).sum()
+retweet_count = data_flat['retweet_count'].resample(AGGREGATE_OFFSET).sum()
+like_count = data_flat['like_count'].resample(AGGREGATE_OFFSET).sum()
+quote_count = data_flat['quote_count'].resample(AGGREGATE_OFFSET).sum()
+tweet_count = data_flat['text'].resample(AGGREGATE_OFFSET).count()
 tweet_count.name='tweet_count'
-print(tweet_count)
-#neu_count = data_flat['text'].resample('1D').count('neu')
-data_agg = pd.merge(reply_count,retweet_count, right_index=True,left_index=True)
+
+#merge columns
+data_agg = pd.merge(text_sent,reply_count, right_index=True,left_index=True)
+data_agg = pd.merge(data_agg,retweet_count, right_index=True,left_index=True)
 data_agg = pd.merge(data_agg,like_count, right_index=True,left_index=True)
 data_agg = pd.merge(data_agg,quote_count, right_index=True,left_index=True)
 data_agg = pd.merge(data_agg,tweet_count, right_index=True,left_index=True)
 #print(data_agg)
 
-data_agg.to_csv(path_or_buf='data.csv', sep=',', index=False)
+#write to file
+data_agg.to_csv(path_or_buf='data.csv', sep=',', index=True)
